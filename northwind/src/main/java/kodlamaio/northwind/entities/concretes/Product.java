032697d5066081d37c4defafee3117535d207716
seval2016 <sevalsenturk2016@gmail.com>
2024-11-18 14:01:50 +0300

    lombok

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,18 +1,31 @@
 package kodlamaio.northwind.entities.concretes;
 
+import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
+@Entity
+@Table(name = "products")
 public class Product {
 
+
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue
     private int id;
+
+    @Column(name = "category_id")
     private int categoryId;
+
+    @Column(name = "product_name")
     private String productName;
+
+    @Column(name = "unit_price")
     private double unitPrice;
+
+    @Column(name = "units_in_stock")
     private short unitsInStock;
+
+    @Column(name = "quantity_per_unit")
     private String quantityPerUnit;
 }
