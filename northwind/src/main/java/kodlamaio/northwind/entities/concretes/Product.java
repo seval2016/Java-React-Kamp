76209e745a94cfa52089f452c0f0a7cc056c009f
seval2016 <sevalsenturk2016@gmail.com>
2024-11-24 00:32:00 +0300

    error choose

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,11 +1,15 @@
 package kodlamaio.northwind.entities.concretes;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
