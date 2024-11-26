7b58bdd0d10c7fb2f77cc4378a37186d2a842c34
seval2016 <sevalsenturk2016@gmail.com>
2024-11-26 14:23:01 +0300

    product jpa code

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -4,7 +4,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.northwind.entities.concretes.Product;
 
+import java.util.List;
+
 public interface ProductDao extends JpaRepository<Product,Integer>{
+
+    Product getByProductName(String productName);
+
+    /**
+     * getByProductNameAndCategoryId metodu, ürün adı ve kategori ID eşleşmesine dayalı olarak tek bir ürün döneceği varsayımıyla Product olarak tanımlanmış
+     * getByProductNameOrCategoryId metodu, daha geniş bir filtreleme yaparak birden fazla ürün dönebileceğinden List<Product>
+     */
+    Product getByProductNameAndCategoryId(String productName, int categoryId);
+
+    List<Product> getByProductNameOrCategoryId(String productName, int categoryId);
+
+    List<Product> getByCategoryId(List<Integer> categories);
+    List<Product> getByProductNameContains(String productName);
+
+    List<Product> getByProductNameStartsWith(String productName);
+
+
 
 
 
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -14,7 +14,6 @@ public class Product {
 
     @Id
     @Column(name = "product_id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     /**
