80a050080927926fe24425facf488ca4439241f5
seval2016 <sevalsenturk2016@gmail.com>
2024-11-27 19:41:40 +0300

    ExceptionHandler api document

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
@@ -2,9 +2,8 @@ package kodlamaio.northwind.api.controllers;
 
 import kodlamaio.northwind.business.abstracts.UserService;
 import kodlamaio.northwind.core.entities.User;
-import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.ErrorDataResult;
-import kodlamaio.northwind.core.utilities.results.Result;
+
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -28,6 +26,18 @@ public class UsersController {
         return ResponseEntity.ok(userService.add(user));
     }
 
+    /**
+     *
+     * @ExceptionHandler:  Bu anotasyon, belirtilen türdeki bir istisna fırlatıldığında ilgili metodu çalıştırır. Burada, MethodArgumentNotValidException yakalanıyor. Bu istisna genellikle, @Valid veya @Validated ile doğrulama yapılırken bir hata oluştuğunda fırlatılır.
+     * @ResponseStatus(HttpStatus.BAD_REQUEST): Metodun döndürdüğü HTTP yanıtının durum kodunu belirler. Burada, 400 Bad Request döndürülüyor, çünkü doğrulama hataları genellikle istemci hatasıdır.
+     * MethodArgumentNotValidException exceptions:MethodArgumentNotValidException, doğrulama hatalarını içeren bir istisnadır. İçerisinde hatalar hakkında detaylı bilgiler bulunur.
+     * getBindingResult(): Form veya API'ye gelen istek üzerindeki tüm doğrulama hatalarını döner.
+     * getFieldErrors(): Hatalı alanların listesine ulaşır.
+     * fieldError.getField(): Hata alınan alanın adını döner. Örneğin: username, password.
+     * fieldError.getDefaultMessage(): Alan için tanımlanan hata mesajını döner. Örneğin: "Bu alan boş bırakılamaz".
+     * Bu işlem sonucunda, hatalı alanları ve hata mesajlarını içeren bir Map (validationErrors) oluşturulur.
+     *
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDataResult<Object> handleValidationException
