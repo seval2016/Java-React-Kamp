871cee325b05c4ea525481a34e6689ebb3b5a74a
seval2016 <sevalsenturk2016@gmail.com>
2024-12-01 23:58:13 +0300

    navi design

--- camp-projectUI/camp-project/src/layouts/Navi.jsx
+++ camp-projectUI/camp-project/src/layouts/Navi.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import CartSummary from "./CartSummary";
 import { MenuMenu, MenuItem, Button, Menu, Container } from "semantic-ui-react";
 import SignedOut from "./SignedOut";
 import SignedIn from "./SignedIn";
 
 export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+  function handleSignOut(params){
+    setIsAuthenticated(false)
+  }
+
+  function handleSignIn(params){
+    setIsAuthenticated(true)
+  }
+
   return (
     <Menu inverted fixed>
       <Container>
@@ -13,8 +23,9 @@ export default function Navi() {
 
         <MenuMenu position="right">
           <CartSummary />
-          <SignedOut/>
-          <SignedIn/>
+          {
+            isAuthenticated ? <SignedIn signOut={handleSignOut}/>:<SignedOut signIn={handleSignIn}/>
+          }
         </MenuMenu>
       </Container>
     </Menu>
