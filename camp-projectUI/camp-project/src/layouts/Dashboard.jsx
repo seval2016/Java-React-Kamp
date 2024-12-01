0dd48f43ae67353099fb37db3a282d16704426fd
seval2016 <sevalsenturk2016@gmail.com>
2024-12-02 00:17:59 +0300

    react-router-dom

--- camp-projectUI/camp-project/src/layouts/Dashboard.jsx
+++ camp-projectUI/camp-project/src/layouts/Dashboard.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import Categories from './Categories'
-import ProductList from './ProductList'
+import React from "react";
+import Categories from "./Categories";
+import ProductList from "./ProductList";
+import { Route } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
 
-export default function Dashboard(){
+export default function Dashboard() {
   return (
     <div>
-       <div className='pageMain'>
-       <div className='categories'><Categories/></div> 
-        <div className='productList'><ProductList/></div>
-       </div>
-        
+      <Grid.Row>
+        <Grid.Column width={4}>
+          <Categories />
+        </Grid.Column>
+        <Grid.Column width={12}>
+          <Route path="/" Component={ProductList} />
+        </Grid.Column>
+      </Grid.Row>
     </div>
-  )
+  );
 }
